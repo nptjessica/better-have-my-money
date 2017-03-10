83cc5c5435595560e6253f3ab53aec12c3d09b8e
Jessica Nguyen <nptjessica@gmail.com>
2017-03-10 19:11:11 +0100

    index.js: when click on a link, open a new tab

--- public/index.js
+++ public/index.js
@@ -1,22 +1,41 @@
-//fill list of Favorites
-function addFavorite(){
-    var table=document.getElementById("table");
-
-    if($("#row1").text().length == 0){
-        document.getElementById("divtable").style.display = "block";
-        document.getElementById("row1").innerHTML = document.getElementById("address").value;
-    }
-    else{
-        var length=table.rows.length;
-        var row=table.insertRow(length);
-        row.innerHTML = document.getElementById("address").value;
-    }
-}
-
 /*if there is at least a row, display table of favorite
 otherwise, when the table is empty, hide table (first time)*/
 function DisplayTableOrNot(){
-    if($("#row1").text().length == 0){
+    if($("#table").text().length == 0){
             document.getElementById("divtable").style.display = "none";
     }
 }
+
+//fill list of Favorites
+function addFavorite(){
+    var table=document.getElementById("table");
+    var favoriteValue=document.getElementById("address").value;
+    var length=table.rows.length;
+    var row=table.insertRow(length);
+
+    if($("#table").text().length == 0){
+        document.getElementById("divtable").style.display = "block";
+        document.getElementById("table").innerHTML ="<a onclick=\"window.open('"+favoriteValue+"');\">"+favoriteValue+"</a>";
+    }
+    else{
+        row.innerHTML = "<a onclick=\"window.open('"+favoriteValue+"');\">"+favoriteValue+"</a>";
+    }
+}
+
+
+/*
+function deleteRow(tableID) {
+    var table = document.getElementById(tableID);
+    var rowCount = table.rows.length;
+
+    for(var i=0; i<rowCount; i++) {
+        var row = table.rows[i];
+        var chkbox = row.cells[0].childNodes[0];
+        if(null != chkbox && true == chkbox.checked) {
+            table.deleteRow(i);
+            rowCount--;
+            i--;
+        }
+    }
+}
+*/
