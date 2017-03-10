9391089e863e3b6894a5737e8e015642a078b615
Jessica Nguyen <nptjessica@gmail.com>
2017-03-10 17:50:12 +0100

    index.js: add Favorite @ in n row

--- public/index.js
+++ public/index.js
@@ -1,23 +1,22 @@
+//fill list of Favorites
 function addFavorite(){
     var table=document.getElementById("table");
-    var length=table.rows.length;
-    var row=table.insertRow(length);
 
     if($("#row1").text().length == 0){
         document.getElementById("divtable").style.display = "block";
         document.getElementById("row1").innerHTML = document.getElementById("address").value;
     }
-    /*else{
-        row.innerHTML = document.getElementById("address").value
-    }*/
-
+    else{
+        var length=table.rows.length;
+        var row=table.insertRow(length);
+        row.innerHTML = document.getElementById("address").value;
+    }
 }
 
-//if there is at least a row, display table of favorite
-//otherwise, when the table is empty, hide table (first time)
+/*if there is at least a row, display table of favorite
+otherwise, when the table is empty, hide table (first time)*/
 function DisplayTableOrNot(){
     if($("#row1").text().length == 0){
             document.getElementById("divtable").style.display = "none";
     }
 }
-
