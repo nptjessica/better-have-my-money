852d44ec9332fa622a72c2feb392228f3ea71f95
Jessica Nguyen <nptjessica@gmail.com>
2017-03-10 17:43:02 +0100

    index.js: add Favorite @ in row1

--- public/index.js
+++ public/index.js
@@ -1,7 +1,23 @@
+function addFavorite(){
+    var table=document.getElementById("table");
+    var length=table.rows.length;
+    var row=table.insertRow(length);
+
+    if($("#row1").text().length == 0){
+        document.getElementById("divtable").style.display = "block";
+        document.getElementById("row1").innerHTML = document.getElementById("address").value;
+    }
+    /*else{
+        row.innerHTML = document.getElementById("address").value
+    }*/
+
+}
+
 //if there is at least a row, display table of favorite
 //otherwise, when the table is empty, hide table (first time)
 function DisplayTableOrNot(){
     if($("#row1").text().length == 0){
-            document.getElementById("table").style.display = "none";
+            document.getElementById("divtable").style.display = "none";
     }
 }
+
