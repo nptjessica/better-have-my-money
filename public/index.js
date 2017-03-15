6718db8af0eea2887088369345fb87f5ed143dc6
Jessica Nguyen <nptjessica@gmail.com>
2017-03-15 16:05:01 +0100

    index.html, index.js, favorite.json: ON GOING display json data,received html input and save into json file

--- public/index.js
+++ public/index.js
@@ -15,10 +15,14 @@ function addFavorite(){
 
     if($("#table").text().length == 0){
         document.getElementById("divtable").style.display = "block";
-        document.getElementById("table").innerHTML ="<a onclick=\"window.open('"+favoriteValue+"');\">"+favoriteValue+"</a>";
+        length = 1;
+        document.getElementById("table").innerHTML ="<td><td><a name=\"id\" onclick=\"window.open('"+favoriteValue+"');\">"+length+"</a></td>"+
+                                                    "<td><a  name=\"address\"onclick=\"window.open('"+favoriteValue+"');\">"+favoriteValue+"</a><td>";
     }
     else{
-        row.innerHTML = "<a onclick=\"window.open('"+favoriteValue+"');\">"+favoriteValue+"</a>";
+        length +=1;
+        row.innerHTML = "<td><td><a name=\"id\"onclick=\"window.open('"+favoriteValue+"');\">"+length+"</a></td>"+
+                        "<td><a name=\"address\"onclick=\"window.open('"+favoriteValue+"');\">"+favoriteValue+"</a><td>";
     }
 }
 
@@ -39,3 +43,40 @@ function deleteRow(tableID) {
     }
 }
 */
+
+
+    /*get the favorite address and put it the JSON file
+    https://code.lengstorf.com/get-form-values-as-json/
+    */
+
+    //find our form in the DOM using its id
+    const form = document.getElementById('#form');
+    //get the form data with our (yet to be defined) function
+    const data = getFormDataJSON(form);
+    //do something with the favorite address
+    doSomething(data.table.address);
+
+    /**
+     * A handler function to prevent default submission and run our custom script.
+     * @param  {Event} event  the submit event triggered by the user
+     * @return {void}
+     */
+    const handleFormSubmit = event => {
+      // Stop the form from submitting since we’re handling that with AJAX.
+      event.preventDefault();
+      // TODO: Call our function to get the form data.
+      const data = {};
+      // Demo only: print the form data onscreen as a formatted JSON object.
+      const dataContainer = document.getElementsById('#table');
+      // Use `JSON.stringify()` to make the output valid, human-readable JSON.
+      dataContainer.textContent = JSON.stringify(data, null, "  ");
+      // ...this is where we’d actually do something with the form data...
+    };
+function handleSubmit(){
+
+}
+
+//display table of favorite from favorite.json file
+function displayFavoriteJSON(){
+    var favorite = loadJSON("favorite.json");
+}
